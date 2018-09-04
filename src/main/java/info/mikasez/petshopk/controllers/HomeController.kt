7fd13ccf15bd0ce8b8ee7815d0795f2e7bc220aa
mikasez <mis.strator@gmail.com>
2018-09-04 22:49:03 +0200

    adding test framework

---
+++ src/main/java/info/mikasez/petshopk/controllers/HomeController.kt
@@ -0,0 +1,16 @@
+package info.mikasez.petshopk.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    fun shop(model : Model) : String{
+        model["title"] = "My PetShop"
+        return "shop"
+    }
+}
---
+++ src/test/java/info/mikasez/petshopk/PetshopkApplicationTests.kt
@@ -0,0 +1,41 @@
+package info.mikasez.petshopk
+
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PetshopkApplicationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+    @Test
+    fun contextLoads() {
+    }
+
+    @Test
+    fun `should run home page with correct title and return code`(){
+        val entity = restTemplate.getForEntity<String>("/")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>My PetShop</h1>")
+
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
+    }
+
+}

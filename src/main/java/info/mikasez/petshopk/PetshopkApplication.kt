36d2c4ca550aab323ebe6318bed58adfcab79a87
mikasez <mis.strator@gmail.com>
2018-09-04 22:04:12 +0200

    Initialisation of the project

---
+++ src/main/java/info/mikasez/petshopk/PetshopkApplication.kt
@@ -0,0 +1,15 @@
+package info.mikasez.petshopk
+
+import org.springframework.boot.Banner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetshopkApplication
+
+fun main(args: Array<String>) {
+    runApplication<PetshopkApplication>(*args) {
+        setBannerMode(Banner.Mode.OFF)
+    }
+}
+
